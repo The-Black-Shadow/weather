fda3fa9245bb378e155ac1b38c70fdc32249461f
The-Black-Shadow <mehedihasan.cse18@gmail.com>
2023-09-11 09:50:57 +0600

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(useMaterial3: true),
+      theme: ThemeData.dark(useMaterial3: true),
       home: const WeatherScreen(),
     );
   }
